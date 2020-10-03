994a7327ec02f363db98fb441cbf9ae3c6a5c69c
mrbanjola <karl@sjoholm.tech>
2020-10-03 21:50:03 +0200

    Audio and controls added

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+
     // Start is called before the first frame update
     void Start()
     {
 
-
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 
     }
-
-    // Update is called once per frame
     void Update()
     {
         ProcessInput();
@@ -21,9 +25,36 @@ public class Rocket : MonoBehaviour
 
     private void ProcessInput()
     {
-        while (Input.GetKey(KeyCode.Space))
+      if (Input.GetKey(KeyCode.Space))
         {
-            print("Le Spacebar");
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+            
+            
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (audioSource.isPlaying)
+            {
+                audioSource.Stop();
+            }
+
+
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+
+            transform.Rotate(Vector3.forward);
+
+        } else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.back);
         }
     }
 }
